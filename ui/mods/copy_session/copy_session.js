bc622f9fb1a1c1799411c6de13810cc38889445b
Justin Love <git@JustinLove.name>
2014-06-10 09:14:46 -0500

    correct message if we only  have session id

--- ui/mods/copy_session/copy_session.js
+++ ui/mods/copy_session/copy_session.js
@@ -1,18 +1,22 @@
 (function() {
 
-  var $message = $('<div id="copy_session">Terminal command copied to clipboard.</div>')
-    .hide().appendTo('body')
-
+  var message = 'Session id'
   var paCommand = ''
   if (navigator.appVersion.match('Mac')) {
+    message = 'Terminal command'
     paCommand = '~/Library/Application\\ Support/Uber\\ Entertainment/Planetary\\ Annihilation/data/streams/stable/PA.app/Contents/MacOS/PA --ticket '
   /* ????
   } else if (navigator.appVersion.match('Linux')) {
+    message = 'Terminal command'
     paCommand = '~/.local/Uber\\ Entertainment/Planetary\\ Annihilation/stable/PA --ticket '
   } else if (navigator.appVersion.match('Win')) {
+    message = 'Terminal command'
     paCommand = '%LOCALAPPDATA%/Uber\\ Entertainment/Planetary\\ Annihilation/data/streams/stable/PA --ticket '
   */
   }
+
+  var $message = $('<div id="copy_session">' + message + ' copied to clipboard.</div>')
+    .hide().appendTo('body')
 
   $('body').on('copy', function(e) {
     if (model.jabberToken() == null || model.jabberToken() === '') return true
