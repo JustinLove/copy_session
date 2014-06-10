9342b9fdd3b54613cf76640c60f6576b8b6dfac4
Justin Love <git@JustinLove.name>
2014-06-10 08:45:53 -0500

    set up skeleton for os detection

--- ui/mods/copy_session/copy_session.js
+++ ui/mods/copy_session/copy_session.js
@@ -1,12 +1,23 @@
 (function() {
 
-  $message = $('<div id="copy_session">Terminal command copied to clipboard.</div>')
+  var $message = $('<div id="copy_session">Terminal command copied to clipboard.</div>')
     .hide().appendTo('body')
+
+  var paCommand = ''
+  if (navigator.appVersion.match('Mac')) {
+    paCommand = '~/Library/Application\\ Support/Uber\\ Entertainment/Planetary\\ Annihilation/data/streams/stable/PA.app/Contents/MacOS/PA --ticket '
+  /* ????
+  } else if (navigator.appVersion.match('Linux')) {
+    paCommand = '~/.local/Uber\\ Entertainment/Planetary\\ Annihilation/stable/PA --ticket '
+  } else if (navigator.appVersion.match('Win')) {
+    paCommand = '%LOCALAPPDATA%/Uber\\ Entertainment/Planetary\\ Annihilation/data/streams/stable/PA --ticket '
+  */
+  }
 
   $('body').on('copy', function(e) {
     if (model.jabberToken() == null || model.jabberToken() === '') return true
 
-    e.originalEvent.clipboardData.setData('Text', '~/Library/Application Support/Uber Entertainment/Planetary Annihilation/data/streams/stable/PA.app/Contents/MacOS/PA --ticket ' + model.jabberToken())
+    e.originalEvent.clipboardData.setData('Text', paCommand + model.jabberToken())
     $message.show().fadeOut(2000)
 
     return false
